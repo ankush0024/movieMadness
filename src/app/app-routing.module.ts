bc42768d5ef8dab8334b6ad6a0a37e9e3953b4cc
ankush024 <48048290+ankush024@users.noreply.github.com>
2020-09-21 17:56:14 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { TestComponent } from './test/test.component';
 const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
+  { path: 'test', component: TestComponent },
   { path: '**', component: PagenotfoundComponent }
 ];
 
