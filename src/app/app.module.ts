16fb330771dbf679b9f76213d34250dcac4883ec
Ankush Singhal <ankush.a.singhal@accenture.com>
2020-11-19 13:42:58 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { TestComponent } from './test/test.component';
-
+import { AboutComponent } from '../components/about/about.component';
+import { PagenotfoundComponent } from '../components/pagenotfound/pagenotfound.component';
+import { TestComponent } from '../components/test/test.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MovieComponent } from '../components/movie/movie.component';
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     PagenotfoundComponent,
-    TestComponent
+    TestComponent,
+    MovieComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
