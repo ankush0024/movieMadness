c1dac67d46aa64371cc32e8d16ac897e8002eb21
Ankush Singhal <71636137+ankush0024@users.noreply.github.com>
2022-11-26 16:35:10 +0530

    Update round.pipe.ts

--- src/app/shared/round.pipe.ts
+++ src/app/shared/round.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: 'roundTen' })
 export class RoundTenPipe implements PipeTransform {
     transform(value: number): number {
-        return Math.round(value / 10) * 10;
+        return Math.round(value);
     }
 }
